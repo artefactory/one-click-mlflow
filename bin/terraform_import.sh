5c31fea85da5760876a1562fca13e128eb5357cb
alexisVLRT <alexisvlrt@gmail.com>
2021-03-16 19:42:18 +0100

    fix: error when no state exists

--- bin/terraform_import.sh
+++ bin/terraform_import.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-app_in_state=$(terraform state list module.mlflow.module.server.google_app_engine_application.app | grep "module.mlflow.module.server.google_app_engine_application.app")
+if terraform state list module.mlflow.module.server.google_app_engine_application.app 1> /dev/null 2> /dev/null ; then
+    app_in_state=$(terraform state list module.mlflow.module.server.google_app_engine_application.app | grep "module.mlflow.module.server.google_app_engine_application.app")
+else
+    app_in_state="exists"
+fi
+
 if [ "$app_exists" != 0 ] && [ "$app_in_state" == "" ]; then
     echo Importing app engine service
     terraform import module.mlflow.module.server.google_app_engine_application.app $TF_VAR_project_id
