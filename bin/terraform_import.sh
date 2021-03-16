be0c2deafb7f7a49300bf592280455c7231fc661
alexisVLRT <alexisvlrt@gmail.com>
2021-03-16 18:57:26 +0100

    fix: check if app is alreadyu in state

--- bin/terraform_import.sh
+++ bin/terraform_import.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$app_exists" != 0 ]; then
+app_in_state=$(terraform state list module.mlflow.module.server.google_app_engine_application.app | grep "module.mlflow.module.server.google_app_engine_application.app")
+if [ "$app_exists" != 0 ] && [ "$app_in_state" == "" ]; then
     echo Importing app engine service
     terraform import module.mlflow.module.server.google_app_engine_application.app $TF_VAR_project_id
 fi
