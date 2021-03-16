b98b6e9507ee0fa157ee13b5fedcbf6c2c40ece7
alexisVLRT <alexisvlrt@gmail.com>
2021-03-16 16:59:57 +0100

    fix: issue when App is activated but no service is deployed

--- bin/terraform_import.sh
+++ bin/terraform_import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TF_VAR_nb_app_engine_services" != "0" ]; then
+if [ "$app_exists" != "0" ]; then
     echo Importing app engine service
     terraform import module.mlflow.module.server.google_app_engine_application.app $TF_VAR_project_id
 fi
