ebbc1e094b62bdbcde2fced77d7bd9af153b2838
alexisVLRT <alexisvlrt@gmail.com>
2021-03-21 14:14:25 +0100

    fix: terraform import

--- bin/terraform_import.sh
+++ bin/terraform_import.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if terraform state list module.mlflow.module.server.google_app_engine_application.app 1> /dev/null 2> /dev/null ; then
-    app_in_state=$(terraform state list module.mlflow.module.server.google_app_engine_application.app | grep "module.mlflow.module.server.google_app_engine_application.app")
+    app_in_state=1
 else
-    app_in_state="exists"
+    app_in_state=0
 fi
 
-if [ "$app_exists" != 0 ] && [ "$app_in_state" == "" ]; then
+if [ "$app_exists" == 1 ] && [ "$app_in_state" == 0 ]; then
     echo Importing app engine service
     terraform import module.mlflow.module.server.google_app_engine_application.app $TF_VAR_project_id
 fi
