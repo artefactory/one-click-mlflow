a31b65e4ea12c17c1a1824871b9cd21cfeedaf25
alexisVLRT <alexisvlrt@gmail.com>
2021-05-07 17:18:14 +0200

    fix: linting

--- bin/set_app_engine.sh
+++ bin/set_app_engine.sh
@@ -11,7 +11,7 @@ fi
 if [[ "$DEPLOYED_VERSIONS" == "" ]]; then
   echo "=> A dummy app engine with the name \"default\" will be created"
   ./save_var.sh TF_VAR_create_default_service 1
-elif [[ "$DEPLOYED_VERSIONS" == *"mlflow-default"* ]];then
+elif [[ "$DEPLOYED_VERSIONS" == *"mlflow-default"* ]]; then
   ./save_var.sh TF_VAR_create_default_service 1
 else
   ./save_var.sh TF_VAR_create_default_service 0
